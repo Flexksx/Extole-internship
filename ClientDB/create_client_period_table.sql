c1cc82ad407699699048745620b60ea9d4e44141
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-09-21 19:58:36 +0300

    added a db manager to add the files to the database

--- ClientDB/create_client_period_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contacts (
-	id INTEGER PRIMARY KEY,
-	period_end TEXT NOT NULL,
-	client_id TEXT NOT NULL,
-	attributed_customers INTEGER,
-	unattributed_customers INTEGER,
-    contribution_rate REAL
-);
