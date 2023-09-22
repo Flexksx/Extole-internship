f163c2bf16e18f2839d3a4040673889d190e5729
VintusS <dragomir.mindrescu@isa.utm.md>
2023-09-22 08:56:49 +0300

    dividing the linechart html file into html and js

--- dashboard.js
+++ dashboard.js
@@ -6,53 +6,49 @@ function toggleMenu() {
         menu.style.display = "block";
     }
 }
+
 const quartersData = {
     Q1: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
     Q2: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
     Q3: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
     Q4: Array.from({ length: 12 }, () => Math.floor(Math.random() * 100)),
 };
-function updateChart(selectedQuarter) {
-    const chart = Highcharts.chart('chart-container', {
-        title: {
-            text: 'Line Chart'
-        },
-        xAxis: {
-            categories: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
-        },
-        yAxis: {
-            title: {
-                text: 'Contribution Rate'
-            }
-        },
-        series: [{
-            name: "CR, %",
-            data: quartersData[selectedQuarter] 
-        }]
-    });
-}
 
-function updateBarChart(selectedQuarter) {
-    const chart = Highcharts.chart('bar-chart-container', {
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Bar Chart'
-        },
-        xAxis: {
-            categories: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
-        },
-        yAxis: {
-            title: {
-                text: 'Contribution Rate'
-            }
-        },
-        series: [{
-            name: "CR, %",
-            data: quartersData[selectedQuarter]
-        }]
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const periodSelect = document.getElementById("period-select");
+    const chartContainer = document.getElementById("chart-container");
 
-updateBarChart('Q1');
+    for (const quarter in quartersData) {
+        const option = document.createElement("option");
+        option.value = quarter;
+        option.textContent = quarter;
+        periodSelect.appendChild(option);
+    }
+
+    periodSelect.value = "Q1";
+    updateChart("Q1");
+
+    periodSelect.addEventListener("change", function() {
+        updateChart(this.value);
+    });
+
+    function updateChart(selectedQuarter) {
+        const chart = Highcharts.chart(chartContainer, {
+            title: {
+                text: 'Line Chart'
+            },
+            xAxis: {
+                categories: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
+            },
+            yAxis: {
+                title: {
+                    text: 'Contribution Rate'
+                }
+            },
+            series: [{
+                name: "CR, %",
+                data: quartersData[selectedQuarter] 
+            }]
+        });
+    }
+});
