ed9c046afa1bead0399bd3c7c0d3691836192003
VintusS <dragomir.mindrescu@isa.utm.md>
2023-10-11 16:29:03 +0300

    -

--- database/database_scripts_py/db_main.py
+++ database/database_scripts_py/db_main.py
@@ -5,16 +5,16 @@ import sqlite3
 
 
 
-conn = create_connection("/home/cristi/Documents/practica/database/ClientDB/Clients.db")
+conn = create_connection("/Users/vintuss/practica-1/database/ClientsDB/Clients.db")
 
 
-drop_tables(conn)
+# drop_tables(conn)
 create_clients_table(conn)
 create_periods_table(conn)
 create_records_table(conn)
 
 print(list_tables(conn))
-client_data,period_data,record_data = load_data_from_json("/home/cristi/Documents/practica/data_analysis/data _aggregation/clients_per_day.json")
+client_data,period_data,record_data = load_data_from_json("/Users/vintuss/practica-1/data_analysis/data _aggregation/clients_per_day.json")
 print(client_data[0],period_data[0],record_data[0])
 insert_to_clients(conn,client_data)
 insert_to_periods(conn,period_data)
