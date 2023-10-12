17c2e38a6d5885ca7a53a19b39a5c2b2b04b0741
VintusS <dragomir.mindrescu@isa.utm.md>
2023-10-12 11:44:46 +0300

    upd dashboard right side

--- src/components/ContributionChart.jsx
+++ src/components/ContributionChart.jsx
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 export function ContributionChart() {
+  const weeks = Array.from({ length: 15 }, (_, index) => `Week ${index + 1}`);
   const options = {
     chart: {
-      type: 'column',
+      type: 'line',
+      height: 400,
     },
     title: {
-      text: 'Contribution Rate Chart',
+      text: 'Dashboard for ClientID:',
+      align: 'left',
     },
-    // Add your Highcharts configuration options here.
+    xAxis: {
+      categories: weeks,
+    },
+    yAxis: {
+      title: {
+        text: 'Contribution Rate',
+      },
+    },
+    series: [
+      {
+        name: 'ClientID 1',
+        data: [20, 30, 25, 40, 45],
+      },
+    ],
+  };
+
+  const [selectedQuarter, setSelectedQuarter] = useState('Quarter 1');
+
+  const handleQuarterChange = (e) => {
+    setSelectedQuarter(e.target.value);
   };
 
   return (
     <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', paddingTop: '10px' }}>
+  <div style={{ backgroundColor: 'red', width: '20%', height: '150px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontWeight: 'bold' }}>
+    <p>Contribution Rate, %</p>
+  </div>
+  <div style={{ width: '20px' }}></div>
+  <div style={{ backgroundColor: 'red', width: '20%', height: '150px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontWeight: 'bold' }}>
+    <p>CR per Source</p>
+  </div>
+  <div style={{ width: '20px' }}></div>
+  <div style={{ backgroundColor: 'red', width: '20%', height: '150px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontWeight: 'bold' }}>
+    <p>Month over Month</p>
+  </div>
+  <div style={{ width: '20px' }}></div>
+  <div style={{ backgroundColor: 'red', width: '20%', height: '150px', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontWeight: 'bold' }}>
+    <p>Quarter over Quarter</p>
+  </div>
+</div>
       <HighchartsReact highcharts={Highcharts} options={options} />
+
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <select value={selectedQuarter} onChange={handleQuarterChange} style={{ width: '200px' }}>
+          <option value="Quarter 1">Quarter 1</option>
+          <option value="Quarter 2">Quarter 2</option>
+          <option value="Quarter 3">Quarter 3</option>
+          <option value="Quarter 4">Quarter 4</option>
+        </select>
+      </div>
     </div>
   );
-};
+}
 
 export default ContributionChart;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Box, Flex, Image, Text, VStack, Link, Icon } from '@chakra-ui/react';
 import { FaChartLine, FaHistory, FaBook, FaCog, FaPhone } from 'react-icons/fa';
-import {ContributionChart }from '../components'; // Import ContributionChart
-
+import { ContributionChart } from '../components';
+import { FaBell, FaUser, FaBars } from 'react-icons/fa';
 const logoUrl =
   'https://4a71b5761700fa86bf84.cdn6.editmysite.com/uploads/b/4a71b5761700fa86bf842ad62ae8665c4f535d423548ebe8cdacd81778f0713f/Extole-logo_large-copy_1656441979.png?width=800&optimize=medium';
 
@@ -20,7 +20,7 @@ export function Dashboard() {
         <Image src={logoUrl} alt="Company Logo" w="75%" mx="auto" mb={4} />
 
         <Box
-          bg="red.500"
+          bg="red"
           borderRadius="md"
           p={2}
           textAlign="center"
@@ -33,26 +33,26 @@ export function Dashboard() {
         <VStack spacing={2} align="center" flex="1">
           <Link display="flex" alignItems="center">
             <Icon as={FaChartLine} />
-            <Text pl={2}>Analytics</Text> {/* Add padding to the left of the text */}
+            <Text>Analytics</Text>
           </Link>
           <Link display="flex" alignItems="center">
             <Icon as={FaHistory} />
-            <Text pl={2}>History</Text> {/* Add padding to the left of the text */}
+            <Text>History</Text>
           </Link>
           <Link display="flex" alignItems="center">
             <Icon as={FaBook} />
-            <Text pl={2}>Records</Text> {/* Add padding to the left of the text */}
+            <Text>Records</Text>
           </Link>
         </VStack>
         
         <VStack spacing={2} mb={4} mt="auto">
           <Link display="flex" alignItems="center">
             <Icon as={FaCog} />
-            <Text pl={2}>Settings</Text> {/* Add padding to the left of the text */}
+            <Text>Settings</Text>
           </Link>
           <Link display="flex" alignItems="center">
             <Icon as={FaPhone} />
-            <Text pl={2}>Contact us</Text> {/* Add padding to the left of the text */}
+            <Text>Contact us</Text>
           </Link>
         </VStack>
       </Box>
@@ -62,11 +62,23 @@ export function Dashboard() {
         <ContributionChart />
       </Box>
 
-      <Box w="15%" bg="white" borderRadius="md" p={4} boxShadow="md">
-        {/* Add your content for the right section here */}
-        {/* Placeholder content for the right side */}
-        <Text fontSize="lg">Right Side Content</Text>
-        <p>This is where you can include additional information or widgets relevant to your application.</p>
+      <Box w="15%" bg="white" borderRadius="md" p={4} boxShadow="md" display="flex" flexDirection="column">
+        <Text fontSize="xl">More</Text>
+
+        <VStack spacing={4} mt={4}>
+          <Link display="flex" alignItems="center">
+            <Icon as={FaBell} />
+            <Text>Notifications</Text>
+          </Link>
+          <Link display="flex" alignItems="center">
+            <Icon as={FaUser} />
+            <Text>Profile</Text>
+          </Link>
+          <Link display="flex" alignItems="center" to="/">
+  <Icon as={FaBars} />
+  <Text>Menu</Text>
+</Link>
+        </VStack>
       </Box>
     </Flex>
   );
