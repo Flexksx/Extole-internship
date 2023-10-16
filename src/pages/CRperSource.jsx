1281993b9dfe8c66b9c0e6874535c6a8c1658b19
VintusS <dragomir.mindrescu@isa.utm.md>
2023-10-16 11:56:12 +0300

    upd dashboard reference link to barchart

--- src/pages/CRperSource.jsx
+++ src/pages/CRperSource.jsx
@@ -3,37 +3,11 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { Box, Flex, Image, Text, VStack, Icon } from '@chakra-ui/react';
 import { FaChartBar, FaHistory, FaBook, FaCog, FaPhone } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 const logoUrl =
   'https://your-logo-url.com';
 
 export function CRperSource() {
-  const barChartOptions = {
-    chart: {
-      type: 'bar', // Change to 'bar' for a bar chart
-      height: 400, // Set the height of the chart
-    },
-    title: {
-      text: 'Dashboard for ClientID:',
-      align: 'left',
-    },
-    xAxis: {
-      categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
-    },
-    yAxis: {
-      title: {
-        text: 'Bar Chart Title',
-      },
-    },
-    series: [
-      {
-        name: 'ClientID 1',
-        data: [20, 30, 25, 40, 45],
-      },
-    ],
-  };
-
   return (
     <Flex w="100%" h="100vh" p={4} bg="gray.100" borderRadius="lg" justifyContent="space-between">
       <Box w="15%" bg="white" borderRadius="md" p={4} boxShadow="md" display="flex" flexDirection="column">
