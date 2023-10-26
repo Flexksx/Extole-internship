ec0a5d730e8cae286c9d3b77dae611316f1a23d2
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-10-26 11:44:16 +0300

    sdlasdad

--- database/database_scripts_py/db_main.py
+++ database/database_scripts_py/db_main.py
@@ -14,7 +14,7 @@ create_records_table(conn)
 
 print(list_tables(conn))
 client_data, period_data, record_data = load_data_from_json(
-    "C:\\Users\\liviu\\Desktop\\extole\\practica\\data_analysis\\clients_per_day.json")
+    "/home/cristi/Documents/practica/data_analysis/data _aggregation/clients_per_day.json")
 print(client_data[0], period_data[0], record_data[0])
 insert_to_clients(conn, client_data)
 insert_to_periods(conn, period_data)
