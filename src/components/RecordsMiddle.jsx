694836996752009211126306e0a7bebe71f082b2
VintusS <dragomir.mindrescu@isa.utm.md>
2023-11-07 12:26:30 +0200

    adding quarter data in top contribution chart

--- src/components/RecordsMiddle.jsx
+++ src/components/RecordsMiddle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import { getClientSourcesByQuarter, getWeeklyData } from '../services/apiService';
+import { getWeeklyData, getClientSourcesByQuarter } from '../services/apiService';
 
 export function RecordsMiddle() {
   const { clientId } = useParams();
@@ -12,37 +12,38 @@ export function RecordsMiddle() {
   useEffect(() => {
     async function fetchData() {
       try {
+        const getQuarterAndWeek = (weekNumber) => {
+          if (weekNumber <= 12) {
+            return `Quarter 1 Week ${weekNumber}`;
+          } else if (weekNumber <= 25) {
+            return `Quarter 2 Week ${weekNumber - 12}`;
+          } else {
+            return `Quarter 3 Week ${weekNumber - 25}`;
+          }
+        };
         let aggregatedSources = {};
-        let weeklyData = [];
-
-        // Fetch data for all three quarters and aggregate the results
         for (const quarter of ['Q1', 'Q2', 'Q3']) {
           const quarterData = await getClientSourcesByQuarter(clientId, quarter);
           quarterData.forEach(({ source, customers }) => {
             aggregatedSources[source] = (aggregatedSources[source] || 0) + customers;
           });
         }
-
-        // Fetch weekly data and filter by client ID
         const allWeeklyData = await getWeeklyData();
-        weeklyData = allWeeklyData.filter(data => data.client_id.toString() === clientId);
+        const clientWeeklyData = allWeeklyData.filter(data => data.client_id.toString() === clientId);
+        const sortedWeeks = clientWeeklyData
+          .sort((a, b) => b.contribution_rate - a.contribution_rate)
+          .slice(0, 10)
+          .map(week => ({
+            week: getQuarterAndWeek(week.week),
+            contributionRate: week.contribution_rate
+          }));
 
-        // Sort and slice the top 10 sources
+        setTopWeeksData(sortedWeeks);
         const sortedSources = Object.entries(aggregatedSources)
           .map(([source, customers]) => ({ source, customers }))
           .sort((a, b) => b.customers - a.customers)
           .slice(0, 10);
 
-        // Sort and slice the top 10 weeks by contribution rate
-        const sortedWeeks = weeklyData
-          .sort((a, b) => b.contribution_rate - a.contribution_rate)
-          .slice(0, 10)
-          .map(week => ({
-            week: `Week ${week.week}`,
-            contributionRate: week.contribution_rate
-          }));
-
-        setTopWeeksData(sortedWeeks);
         setTopSourcesData(sortedSources);
       } catch (error) {
         console.error("Failed to fetch data", error);
@@ -53,15 +54,24 @@ export function RecordsMiddle() {
   }, [clientId]);
 
   const weeksChartOptions = {
-    chart: { type: 'column' , height: "40%"},
-    title: { text: 'Top Weeks by Contribution Rate' },
+    chart: {
+      type: 'column',
+      height: '43%'
+    },
+    title: {
+      text: 'Top Weeks by Contribution Rate'
+    },
     xAxis: {
       categories: topWeeksData.map(data => data.week),
-      title: { text: 'Week' }
+      title: {
+        text: 'Quarter/Week'
+      }
     },
     yAxis: {
       min: 0,
-      title: { text: 'Contribution Rate (%)' }
+      title: {
+        text: 'Contribution Rate (%)'
+      }
     },
     series: [{
       name: 'Contribution Rate',
@@ -71,15 +81,24 @@ export function RecordsMiddle() {
   };
 
   const sourcesChartOptions = {
-    chart: { type: 'column' , height: "40%"},
-    title: { text: 'Top Sources by Number of Customers' },
+    chart: {
+      type: 'column',
+      height: '43%'
+    },
+    title: {
+      text: 'Top Sources by Number of Customers'
+    },
     xAxis: {
       categories: topSourcesData.map(data => data.source),
-      title: { text: 'Source' }
+      title: {
+        text: 'Source'
+      }
     },
     yAxis: {
       min: 0,
-      title: { text: 'Number of Customers' }
+      title: {
+        text: 'Number of Customers'
+      }
     },
     series: [{
       name: 'Customers',
